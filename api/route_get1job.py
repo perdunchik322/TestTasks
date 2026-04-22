a95754d511a38f1d7e522ed04c9c256d3b979f74
perdun <andrejnnefedov72@gmail.com>
2026-04-23 02:38:18 +0300

    solution5-2

--- api/route_get1job.py
+++ api/route_get1job.py
@@ -1,4 +1,14 @@
-from flask import render_template
+from flask import jsonify, render_template
 from data import db_session
+from flask import jsonify
 from .blueprint import jobs_blueprint
 from data.jobs import Jobs
+
+@jobs_blueprint.route('/jobs/<int:job_id>', methods=['GET'])
+def get_job(job_id):
+    db_sess = db_session.create_session()
+    job = db_sess.query(Jobs).get(job_id)
+    if not job:
+        return jsonify({'error': 'Job not found'}), 404
+    
+    return jsonify({'job': job.to_dict()})
